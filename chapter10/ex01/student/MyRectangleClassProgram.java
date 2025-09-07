9121d6de435eb2fcfbab9192581fda3b241e9515
rossellveras <rossell.veras@students.post.edu>
2025-09-07 16:58:10 -0400

    10-1 Rossell Veras

--- chapter10/ex01/student/MyRectangleClassProgram.java
+++ chapter10/ex01/student/MyRectangleClassProgram.java
@@ -6,16 +6,38 @@ public class MyRectangleClassProgram
    {
       // Create rectangle1 and rectangle2 objects here
 
-      // Set the length of rectangle1 to 10.0 here
+        Rectangle rectangle1 = new Rectangle();
+        Rectangle rectangle2 = new Rectangle();
 
+      // Set the length of rectangle1 to 10.0 here
       // Set the width of rectangle1 to 5.0 here
+
+         rectangle1.setLength(10.0);
+         rectangle1.setWidth(5.0);
 
       // Print the area and perimeter of rectangle1 here
 
-      // Set the length of rectangle2 to 7.0 here
+        System.out.println("Rectangle 1:");                
+        System.out.println("Length: " + rectangle1.getLength());  
+        System.out.println("Width: " + rectangle1.getWidth());      
+        System.out.println("Perimeter: " + rectangle1.calculatePerimeter()); 
+        System.out.println("Area: " + rectangle1.calculateArea());         
 
+        System.out.println(); // Blank line to separate rectangles
+
+      // Set the length of rectangle2 to 7.0 here
       // Set the width of rectangle2 to 3.0 here 
 
+      rectangle2.setLength(7.0);  
+      rectangle2.setWidth(3.0);
+
       // Print the area and perimeter of rectangle2 here 
+       System.out.println("Rectangle 2:");                 
+        System.out.println("Length: " + rectangle2.getLength());    
+        System.out.println("Width: " + rectangle2.getWidth());       
+        System.out.println("Perimeter: " + rectangle2.calculatePerimeter()); 
+        System.out.println("Area: " + rectangle2.calculateArea()); 
    }
+
+
 }
--- chapter10/ex01/student/Rectangle.java
+++ chapter10/ex01/student/Rectangle.java
@@ -1,17 +1,39 @@
 class Rectangle
 {
 	// Length of this rectangle
+    private double length;
+
 	// Width of this rectangle
-	
+	private double width;
 
-	// Write set methods here
-	
+	// Set method for length
+	public void setLength(double length) {
+    this.length = length;
+}
 
+   //Get method for length
+	public double getLength() {
+    return length;
 
-	// Write get methods here
+// --- Set method for width ---
+public void setWidth(double width) {
+    this.width = width;
+}
 
+// --- Get method for width ---
+  public double getWidth() {
+    return width;
 	
 
 	// Write the calculatePerimeter() and 
 	// calculateArea() methods here	
+
+   public double calculateArea() {
+    return length * width;
+}
+
+	public double calculatePerimeter() {
+    return 2 * (length + width);
+
+
 }
