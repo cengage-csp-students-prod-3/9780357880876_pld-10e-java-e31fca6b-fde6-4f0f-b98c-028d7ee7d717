908eb7ec8765b5f50d9b0c34546b9eeaa6b4a348
rossellveras <rossell.veras@students.post.edu>
2025-09-14 15:48:50 +0000

    Testing Sun Sep 14 2025 15:48:50 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/Rectangle.java
+++ chapter10/ex01/student/Rectangle.java
@@ -2,24 +2,24 @@ public class Rectangle {
     private double length;
     private double width;
 
-    // Default constructor
-	
+  
+
     public Rectangle() {
         length = 0.0;
         width = 0.0;
     }
 
-    // ✅ SETTER for length
+  
     public void setLength(double len) {
         length = len;
     }
 
-    // ✅ GETTER for length
+  
     public double getLength() {
         return length;
     }
 
-    // Optional: Set and get methods for width (if needed)
+    
     public void setWidth(double w) {
         width = w;
     }
@@ -28,7 +28,7 @@ public class Rectangle {
         return width;
     }
 
-    // Optional: Methods to calculate area and perimeter
+    
     public double calculateArea() {
         return length * width;
     }
