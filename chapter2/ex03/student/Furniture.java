ba97f3981d9e7a3165d69d67a76db6504b6c9393
rossellveras <rossell.veras@students.post.edu>
2025-09-14 17:59:32 +0000

    Testing Sun Sep 14 2025 17:59:32 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/Furniture.java
+++ chapter2/ex03/student/Furniture.java
@@ -11,7 +11,8 @@ public class Furniture
 		double profit;
 		double saleProfit;
 		
-		// Write your assignment statements here.			
+		// Write your assignment statements here.	
+				
 		profit = retailPrice - wholesalePrice;
 
 		salePrice = retailPrice * 0.75;
