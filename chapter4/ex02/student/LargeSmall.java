843a95703f42119b79caf0be799713065f9989a5
rossellveras <rossell.veras@students.post.edu>
2025-09-20 20:12:45 +0000

    Testing Sat Sep 20 2025 20:12:45 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/LargeSmall.java
+++ chapter4/ex02/student/LargeSmall.java
@@ -1,18 +1,39 @@
-// LargeSmall.java - This program calculates the largest and smallest of three integer values. 
-
 
 public class LargeSmall 
 {
-	public static void main(String args[])
-	{
-		// Declare and initialize variables here.
-		int largest;		// Largest of the three values.
-		int smallest;		// Smallest of the three values.
-		
-		//Write assignment, if, or if else statements here as appropriate.	
-		
+    public static void main(String args[])
+    {
+        // Three numbers are already declared
+        int firstNumber = 78;
+        int secondNumber = -50;
+        int thirdNumber = 12;
+
+        int largest;    // Already declared for largest value
+        int smallest;   // Already declared for smallest value
+
+        // Determine the largest value
+        if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+            largest = firstNumber;
+        } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+            largest = secondNumber;
+        } else {
+            largest = thirdNumber;
+        }
+
+        // Determine the smallest value
+        if (firstNumber <= secondNumber && firstNumber <= thirdNumber) {
+            smallest = firstNumber;
+        } else if (secondNumber <= firstNumber && secondNumber <= thirdNumber) {
+            smallest = secondNumber;
+        } else {
+            smallest = thirdNumber;
+        }
+
+
 		// Output largest and smallest number. 
-		System.out.println("The largest value is " + largest);
-		System.out.println("The smallest value is " + smallest);
-	}
-} 
+			System.out.println("The largest value is " + largest);
+			System.out.println("The smallest value is " + smallest);
+		
+		
+		}
+	} 
