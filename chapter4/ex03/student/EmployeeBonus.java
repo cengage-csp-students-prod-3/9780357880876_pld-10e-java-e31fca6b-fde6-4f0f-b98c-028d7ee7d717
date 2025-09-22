1410480dcdab1a2cc3095c71e1e4aebe7985bfb6
rossellveras <rossell.veras@students.post.edu>
2025-09-22 00:44:17 +0000

    Testing Mon Sep 22 2025 00:44:17 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/EmployeeBonus.java
+++ chapter4/ex03/student/EmployeeBonus.java
@@ -4,36 +4,53 @@ import javax.swing.*;
 
 public class EmployeeBonus 
 {
-	public static void main(String args[])
-	{
-		// Declare and initialize variables here.
-		String employeeName; 
-		double numTransactions; 
-		String transactString;
-		double numShifts; 
-		String shiftString;
-		double dollarValue;
-		String dollarString;
-		double score;
-		double bonus;
-		final double BONUS_1 = 50.00;
-		final double BONUS_2 = 75.00;
-		final double BONUS_3 = 100.00; 
-		final double BONUS_4 = 200.00;		
-				
-      employeeName = JOptionPane.showInputDialog("Enter employee's name: ");
-		shiftString = JOptionPane.showInputDialog("Enter number of shifts: ");
-		transactString = JOptionPane.showInputDialog("Enter number of transactions: ");
-		dollarString = JOptionPane.showInputDialog("Enter transactions dollar value: ");
+    public static void main(String args[])
+    {
+        // Declare and initialize variables here.
+        String employeeName; 
+        double numTransactions; 
+        String transactString;
+        double numShifts; 
+        String shiftString;
+        double dollarValue;
+        String dollarString;
+        double score;
+        double bonus;
+        final double BONUS_1 = 50.00;
+        final double BONUS_2 = 75.00;
+        final double BONUS_3 = 100.00; 
+        final double BONUS_4 = 200.00;		
 
-		numShifts = Double.parseDouble(shiftString);
-		numTransactions = Double.parseDouble(transactString);
-		dollarValue = Double.parseDouble(dollarString);
-		
-		// Write your code here
-		
-		// Output.  
-		System.out.println("Employee Name: " + employeeName);
-		System.out.println("Employee Bonus: $" + bonus);
-	}
-} 
+        employeeName = JOptionPane.showInputDialog("Enter employee's name: ");
+        shiftString = JOptionPane.showInputDialog("Enter number of shifts: ");
+        transactString = JOptionPane.showInputDialog("Enter number of transactions: ");
+        dollarString = JOptionPane.showInputDialog("Enter transactions dollar value: ");
+
+        numShifts = Double.parseDouble(shiftString);
+        numTransactions = Double.parseDouble(transactString);
+        dollarValue = Double.parseDouble(dollarString);
+        
+        // ---- Write your code here ----
+        score = (dollarValue / numTransactions) / numShifts;
+
+        // Nested if statements to decide bonus
+        if (score <= 30)
+            bonus = BONUS_1;
+        else
+        {
+            if (score <= 69)
+                bonus = BONUS_2;
+            else
+            {
+                if (score <= 199)
+                    bonus = BONUS_3;
+                else
+                    bonus = BONUS_4;
+            }
+        }
+
+        // Output  
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee Bonus: $" + bonus);
+    }
+}
