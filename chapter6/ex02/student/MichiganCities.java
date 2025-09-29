37657bb5b7dfff844e90b7b9a4ac599df9ee60e3
rossellveras <rossell.veras@students.post.edu>
2025-09-29 17:51:06 +0000

    Testing Mon Sep 29 2025 17:51:06 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/MichiganCities.java
+++ chapter6/ex02/student/MichiganCities.java
@@ -28,6 +28,7 @@ public class MichiganCities
             }
         }
         
+        
         // Task 2: Test to see if city was not found
         if (foundIt)
             JOptionPane.showMessageDialog(null, "City found.");
