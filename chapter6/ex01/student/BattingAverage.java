25d60540274118bd2d7ac9df23e908f4547fcde0
rossellveras <rossell.veras@students.post.edu>
2025-09-29 15:30:33 +0000

    Testing Mon Sep 29 2025 15:30:33 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/BattingAverage.java
+++ chapter6/ex01/student/BattingAverage.java
@@ -1,64 +1,14 @@
-import javax.swing.*;
+#include <iostream>
+#include <string>
+using namespace std;
 
-public class BattingAverage {
-    public static void main(String args[]) {
-        // Declare a named constant for array size here.
-        final int ARRAY_SIZE = 8;
+int main()
+{
+   // Declare a named constant for array size here   
+   const int ARRAY_SIZE = 8;
 
-        // Declare array here.
-        double[] averages = new double[ARRAY_SIZE];
+   // Declare array here
+   double averages[ARRAY_SIZE];
 
-        // Use this variable to store the batting average input by user.
-        double battingAverage;
+   // Use this var
 
-        // String version of batting average input by user.
-        String averageString;
-
-        // Use these variables to store the minimum and maximum batting averages.
-        double min, max;
-
-        // Use these variables to store the total and the average.
-        double total, average;
-
-        // Write a loop to get batting averages from user and assign to array.
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            averageString = JOptionPane.showInputDialog("Enter a batting average: ");
-            battingAverage = Double.parseDouble(averageString);
-            // Assign value to array.
-            averages[i] = battingAverage;
-        }
-
-        // Assign the first element in the array to be the minimum and the maximum.
-        min = averages[0];
-        max = averages[0];
-        // Start out your total with the value of the first element in the array.
-        total = averages[0];
-
-        // Write a loop here to access array values starting with averages[1]
-        for (int i = 1; i < ARRAY_SIZE; i++) {
-            // Within the loop test for minimum and maximum batting averages.
-            if (averages[i] < min) {
-                min = averages[i];
-            }
-            if (averages[i] > max) {
-                max = averages[i];
-            }
-
-            // Also accumulate a total of all batting averages.
-            total += averages[i];
-        }
-
-        // Calculate the average of the 8 averages.
-        average = total / ARRAY_SIZE;
-
-        // Print the averages stored in the averages array.
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            System.out.println("averages[" + i + "] is: " + averages[i]);
-        }
-
-        // Print the maximum batting average, minimum batting average, and average batting average.
-        System.out.println("Minimum batting average is " + min);
-        System.out.println("Maximum batting average is " + max);
-        System.out.println("Average batting average is " + average);
-    }
-}
--- chapter6/ex02/student/MichiganCities.java
+++ chapter6/ex02/student/MichiganCities.java
@@ -6,23 +6,32 @@ import javax.swing.*;
 
 public class MichiganCities
 {
-	public static void main(String args[]) throws Exception
-	{
-		// Declare variables.
-		String inCity;  // name of city to look up in array.
-		// Initialized array of cities in Michigan.
-		String citiesInMichigan[] = {"Acme", "Albion", "Detroit", "Watervliet", "Coloma", "Saginaw", "Richland", "Glenn", "Midland", "Brooklyn"}; 
-		boolean foundIt = false;  // Flag variable.
+    public static void main(String args[]) throws Exception
+    {
+        // Declare variables.
+        String inCity;  // name of city to look up in array.
+        // Initialized array of cities in Michigan.
+        String citiesInMichigan[] = {"Acme", "Albion", "Detroit", "Watervliet", "Coloma", "Saginaw", "Richland", "Glenn", "Midland", "Brooklyn"}; 
+        boolean foundIt = false;  // Flag variable.
 
-		// Get user input.
-		inCity = JOptionPane.showInputDialog("Enter name of city: ");
-			
-		// Write your loop here. 	
-		
-			// Write your test statement here to see if there is 
-			// a match. Set the flag to true if city is found. 	
-			
-		// Test to see if city was not found to determine if 
-		// "Not a city in Michigan." message should be printed. 
-	} 
-} 
+        // Get user input.
+        inCity = JOptionPane.showInputDialog("Enter name of city: ");
+            
+        // Task 1: Write your loop here.
+        for (int i = 0; i < citiesInMichigan.length; i++) 
+        {
+            // Task 2: Write your test statement here.
+            if (inCity.equals(citiesInMichigan[i])) 
+            {
+                foundIt = true;
+                break; // no need to keep searching
+            }
+        }
+        
+        // Task 2: Test to see if city was not found
+        if (foundIt)
+            JOptionPane.showMessageDialog(null, "City found.");
+        else
+            JOptionPane.showMessageDialog(null, "Not a city in Michigan.");
+    } 
+}
