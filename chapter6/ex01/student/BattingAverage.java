215fbb9c96893cbe230a7e05bc5f8181cd1f6bab
rossellveras <rossell.veras@students.post.edu>
2025-09-29 14:45:33 +0000

    Testing Mon Sep 29 2025 14:45:33 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/BattingAverage.java
+++ chapter6/ex01/student/BattingAverage.java
@@ -1,50 +1,64 @@
 import javax.swing.*;
-public class BattingAverage
-{
-	public static void main(String args[])
-	{
-		// Declare a named constant for array size here.
 
-		// Declare array here.
-		
-		// Use this variable to store the batting average input by user.
-		double battingAverage;
+public class BattingAverage {
+    public static void main(String args[]) {
+        // Declare a named constant for array size here.
+        final int ARRAY_SIZE = 8;
 
-		// String version of batting average input by user.
-		String averageString; 
+        // Declare array here.
+        double[] averages = new double[ARRAY_SIZE];
 
-		// Use these variables to store the minimim and maximum batting averages.
-		double min, max;
+        // Use this variable to store the batting average input by user.
+        double battingAverage;
 
-		// Use these variables to store the total and the average.
-		double total, average; 
+        // String version of batting average input by user.
+        String averageString;
 
-		// Write a loop to get batting averages from user and assign to array.
-		
-		   averageString = JOptionPane.showInputDialog("Enter a batting average: ");
-		   battingAverage = Double.parseDouble(averageString);
-		   // Assign value to array.
-		   
-		
-		// Assign the first element in the array to be the minimum and the maximum.
-		min = averages[0];
-		max = averages[0];
-		// Start out your total with the value of the first element in the array.
-		total = averages[0]; 
-		
-		// Write a loop here to access array values starting with averages[1]
-		
-		   // Within the loop test for minimum and maximum batting averages.
-		   
-		   // Also accumulate a total of all batting averages.
-		   
-		
-		   
-		// Calculate the average of the 8 averages.
-		
+        // Use these variables to store the minimum and maximum batting averages.
+        double min, max;
 
-		// Print the averages stored in the averages array. 
-		
-		// Print the maximum batting average, minimum batting average, and average batting average. 
-	}
+        // Use these variables to store the total and the average.
+        double total, average;
+
+        // Write a loop to get batting averages from user and assign to array.
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            averageString = JOptionPane.showInputDialog("Enter a batting average: ");
+            battingAverage = Double.parseDouble(averageString);
+            // Assign value to array.
+            averages[i] = battingAverage;
+        }
+
+        // Assign the first element in the array to be the minimum and the maximum.
+        min = averages[0];
+        max = averages[0];
+        // Start out your total with the value of the first element in the array.
+        total = averages[0];
+
+        // Write a loop here to access array values starting with averages[1]
+        for (int i = 1; i < ARRAY_SIZE; i++) {
+            // Within the loop test for minimum and maximum batting averages.
+            if (averages[i] < min) {
+                min = averages[i];
+            }
+            if (averages[i] > max) {
+                max = averages[i];
+            }
+
+            // Also accumulate a total of all batting averages.
+            total += averages[i];
+        }
+
+        // Calculate the average of the 8 averages.
+        average = total / ARRAY_SIZE;
+
+        // Print the averages stored in the averages array.
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            System.out.println("averages[" + i + "] is: " + averages[i]);
+        }
+
+        // Print the maximum batting average, minimum batting average, and average batting average.
+        System.out.println("Minimum batting average is " + min);
+        System.out.println("Maximum batting average is " + max);
+        System.out.println("Average batting average is " + average);
+    }
 }
