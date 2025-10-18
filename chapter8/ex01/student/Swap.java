f2db2c78ba1f275aa9724d764ec90ad13f50c816
rossellveras <rossell.veras@students.post.edu>
2025-10-18 21:52:58 +0000

    Testing Sat Oct 18 2025 21:52:58 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex01/student/Swap.java
+++ chapter8/ex01/student/Swap.java
@@ -2,52 +2,80 @@ public class Swap
 {
     public static void main(String[] args)
     {
+        int first, second, third, temp;
+
         // --- Test Set 1: 101, 22, -23 ---
-        int first = 101;
-        int second = 22;
-        int third = -23;
-        sortAndDisplay(first, second, third);
+        first = 101;
+        second = 22;
+        third = -23;
 
-        // --- Test Set 2: 630, 1500, 9 ---
-        first = 630;
-        second = 1500;
-        third = 9;
-        sortAndDisplay(first, second, third);
-
-        // --- Test Set 3: 21, 2, 2 ---
-        first = 21;
-        second = 2;
-        third = 2;
-        sortAndDisplay(first, second, third);
-    }
-
-    public static void sortAndDisplay(int first, int second, int third)
-    {
-        int temp;
-
-        // Compare and swap
         if (first > second) {
             temp = first;
             first = second;
             second = temp;
         }
-
         if (second > third) {
             temp = second;
             second = third;
             third = temp;
         }
+        if (first > second) {
+            temp = first;
+            first = second;
+            second = temp;
+        }
+        System.out.println("Smallest: " + first);
+        System.out.println("Next largest: " + second);
+        System.out.println("Largest: " + third);
+        System.out.println();
+
+        // --- Test Set 2: 630, 1500, 9 ---
+        first = 630;
+        second = 1500;
+        third = 9;
 
         if (first > second) {
             temp = first;
             first = second;
             second = temp;
         }
-
-        // Display results
+        if (second > third) {
+            temp = second;
+            second = third;
+            third = temp;
+        }
+        if (first > second) {
+            temp = first;
+            first = second;
+            second = temp;
+        }
         System.out.println("Smallest: " + first);
         System.out.println("Next largest: " + second);
         System.out.println("Largest: " + third);
-        System.out.println(); // Blank line between each set
+        System.out.println();
+
+        // --- Test Set 3: 21, 2, 2 ---
+        first = 21;
+        second = 2;
+        third = 2;
+
+        if (first > second) {
+            temp = first;
+            first = second;
+            second = temp;
+        }
+        if (second > third) {
+            temp = second;
+            second = third;
+            third = temp;
+        }
+        if (first > second) {
+            temp = first;
+            first = second;
+            second = temp;
+        }
+        System.out.println("Smallest: " + first);
+        System.out.println("Next largest: " + second);
+        System.out.println("Largest: " + third);
     }
 }
