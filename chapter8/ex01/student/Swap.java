de04c6ff6ea1056aab5eceed986074d568075c14
rossellveras <rossell.veras@students.post.edu>
2025-10-18 21:50:32 +0000

    Testing Sat Oct 18 2025 21:50:32 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex01/student/Swap.java
+++ chapter8/ex01/student/Swap.java
@@ -2,19 +2,19 @@ public class Swap
 {
     public static void main(String[] args)
     {
-        // --- Test Set 1 ---
+        // --- Test Set 1: 101, 22, -23 ---
         int first = 101;
         int second = 22;
         int third = -23;
         sortAndDisplay(first, second, third);
 
-        // --- Test Set 2 ---
+        // --- Test Set 2: 630, 1500, 9 ---
         first = 630;
         second = 1500;
         third = 9;
         sortAndDisplay(first, second, third);
 
-        // --- Test Set 3 ---
+        // --- Test Set 3: 21, 2, 2 ---
         first = 21;
         second = 2;
         third = 2;
@@ -25,6 +25,7 @@ public class Swap
     {
         int temp;
 
+        // Compare and swap
         if (first > second) {
             temp = first;
             first = second;
@@ -43,11 +44,10 @@ public class Swap
             second = temp;
         }
 
+        // Display results
         System.out.println("Smallest: " + first);
         System.out.println("Next largest: " + second);
         System.out.println("Largest: " + third);
-        System.out.println(); // Blank line between sets
+        System.out.println(); // Blank line between each set
     }
 }
-
-
