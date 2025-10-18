2d76622afe45ef3910b8cefa67ccf2e4ddebcdbe
rossellveras <rossell.veras@students.post.edu>
2025-10-18 23:23:06 +0000

    Testing Sat Oct 18 2025 23:23:06 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex02/student/HouseholdSize.java
+++ chapter8/ex02/student/HouseholdSize.java
@@ -1,57 +1,65 @@
-// HouseholdSize.java - This program uses a bubble sort to arrange up to 300 household sizes in
-// descending order and then prints the mean and median household size. 
-// Input:  Interactive.
-// Output:  Mean and median household size. 
-
 import javax.swing.*;
 
 public class HouseholdSize
 {
-	public static void main(String args[]) 
-	{
-		// Declare variables.
-		
-		final int SIZE = 300;			// Maximum number of household sizes.
-		int householdSizes[] = new int[SIZE];   // Array used to store up to 300 household sizes.
-		int x;
-		int limit = SIZE;
-		int householdSize = 0; 
-		String householdSizeString;
-		int pairsToCompare;
-		boolean switchOccurred;
-		int temp;
-		double sum = 0; 
-		double mean = 0;
-		int median = 0;
+    public static void main(String args[]) 
+    {
+        final int SIZE = 300;			// Maximum number of household sizes.
+        int householdSizes[] = new int[SIZE];   // Array to store household sizes.
+        int x = 0;                            // Counter for number of entries
+        int householdSize = 0; 
+        String householdSizeString;
+        int temp;
+        double sum = 0; 
+        double mean = 0;
+        double median = 0;
 
-		// Input household size
-		householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
-		householdSize = Integer.parseInt(householdSizeString);
-		// This is the work done in the fillArray() method
-		x = 0;
-		while(x < limit && householdSize != 999)   
-		{
-			// Place value in array.
-			householdSizes[x] = householdSize;
-			// Calculate total of household sizes here
-			
-			x++;    // Get ready for next input item.
-			householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
-			householdSize = Integer.parseInt(householdSizeString); 
-		}  // End of input loop.
-		
-		
-		// Find the mean
-		
-		// This is the work done in the sortArray() method
-		
-		
-		// This is the work done in the displayArray() method
-		// Print the mean 
+        // Input household size
+        householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
+        householdSize = Integer.parseInt(householdSizeString);
 
-		// Calculate the median
+        while(x < SIZE && householdSize != 999)   
+        {
+            // Place value in array
+            householdSizes[x] = householdSize;
+            // Add to total sum for mean
+            sum += householdSize;
+            x++;    // Next input
+            householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
+            householdSize = Integer.parseInt(householdSizeString); 
+        }
 
-      // Print the median
+        int numEntries = x; // total number of valid entries
 
-	} // End of main() method.
-} // End of HouseholdSize class.
+        // Calculate the mean
+        if(numEntries > 0)
+            mean = sum / numEntries;
+
+        // Bubble sort the array in ascending order
+        for(int i = 0; i < numEntries - 1; i++)
+        {
+            for(int j = 0; j < numEntries - i - 1; j++)
+            {
+                if(householdSizes[j] > householdSizes[j+1])
+                {
+                    temp = householdSizes[j];
+                    householdSizes[j] = householdSizes[j+1];
+                    householdSizes[j+1] = temp;
+                }
+            }
+        }
+
+        // Calculate the median
+        if(numEntries > 0)
+        {
+            if(numEntries % 2 == 0) // even number of entries
+                median = (householdSizes[numEntries/2 - 1] + householdSizes[numEntries/2]) / 2.0;
+            else // odd number of entries
+                median = householdSizes[numEntries/2];
+        }
+
+        // Print the results
+        System.out.println("Mean household size in Marengo is : " + mean);
+        System.out.println("Median household size in Marengo is: " + median);
+    }
+}
